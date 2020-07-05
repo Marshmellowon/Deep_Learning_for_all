6463aed12970ff38736d000cb879da1cac668fc7
Marshmellowon <leo503801@gmail.com>
2020-07-05 20:54:35 +0900

    push: add TODO

--- fileReader_data_Regression.py
+++ fileReader_data_Regression.py
@@ -3,15 +3,18 @@ import os
 import tensorflow.compat.v1 as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import numpy as np
+filename_queue = tf.train.string_input_producer(
+    ['data-01-test-score.csv'], shuffle=False, name='filename_queue'
+)
 
-# 타입이 같을때 loadtxt
-xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
-x_data = xy[:, 0:-1]
-y_data = xy[:, [-1]]
+reader = tf.TextLineReader()
+key, value = reader.read(filename_queue)
 
-print(x_data.shape, "\n", x_data, len(x_data), "\n")
-print(y_data.shape, y_data)
+record_defaults = [[0.], [0.], [0.], [0.]]
+xy = tf.decode_csv(value, record_defaults=record_defaults)
+
+# collect batches of csv in
+train_x_batch, train_y_batch = tf.train.batch([xy[0:-1], xy[-1:]], batch_size=10)
 
 # placeholders for a tensor that will be always fed.
 X = tf.placeholder(tf.float32, shape=[None, 3])
@@ -35,12 +38,20 @@ sess = tf.Session()
 # Initializes global variables in the graph.
 sess.run(tf.global_variables_initializer())
 
+# start populating the filename queue.
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
 for step in range(2001):
+    x_batch, y_batch = sess.run([train_x_batch, train_y_batch])
     cost_val, hy_val, _ = sess.run(
-        [cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
+        [cost, hypothesis, train],
+        feed_dict={X: x_batch, Y: y_batch}
+    )
     if step % 10 == 0:
         print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
 
-print("Your score will be ", sess.run(hypothesis, feed_dict={X: [[100, 70, 101]]}))
+coord.request_stop()
+coord.join(threads)
 
-print("Other scores will be\n ", sess.run(hypothesis, feed_dict={X: [[60, 70, 110], [90, 100, 80]]}))
+# TODO: logistic classification
